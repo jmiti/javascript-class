f5a702017825379a40e312c70de284bcfab128f6
Janet Miti <janetmiti@Janets-MBP.lan>
2023-02-28 15:51:31 +0300

    initial commit

---
+++ code.js
@@ -0,0 +1,19 @@
+let first = 3
+if (first == 1) {
+    // console.log("one")
+}
+else { console.log(2) }
+
+let second = (first == 1) ? "one" : 2
+// console.log(second)
+
+switch (first) {
+    case 1:
+        console.log("one")
+        break;
+    case 2:
+        console.log("two")
+        break;
+    default:
+        console.log("three")
+}
